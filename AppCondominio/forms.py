01eae53c099eaa62f973119ab2e513aa678ede98
unknown <Rubensspfc100@gmail.com>
2018-05-30 18:40:43 -0300

    templates e views

--- AppCondominio/forms.py
+++ AppCondominio/forms.py
@@ -1,8 +1,18 @@
-from .models import Apartamento
+from .models import Apartamento,Proprietario,Condominio
 from django import forms
 
 
-class ApartamentoForm(forms.ModelForm):
+class apartamentoForm(forms.ModelForm):
 	class Meta:
 		model=Apartamento
 		fields = '__all__' 
+
+class proprietarioForm(forms.ModelForm):
+	class Meta:
+		model=Proprietario
+		fields='__all__'
+
+class condominioForm(forms.ModelForm):
+	class Meta:
+		model=Condominio
+		fields='__all__'
--- AppCondominio/views.py
+++ AppCondominio/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-from .forms import ApartamentoForm
+from .forms import apartamentoForm,proprietarioForm,condominioForm
 from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 # Create your views here.
 
 def index(request):
@@ -12,5 +13,31 @@ def boot(request):
 	return render(request,'teste_bootstrap.html')
 
 def apartForm(request):
-	form=ApartamentoForm()
+	form=apartamentoForm(request.POST or None)
+	if request.method=='POST':
+		if form.is_valid():
+			form.save()
+		else:
+			form=apartamentoForm()
 	return render(request, 'formulario_teste.html',{'form':form})
+
+def proprietarioFormulario(request):
+	formProprietario=proprietarioForm(request.POST or None)
+	if request.method=='POST':
+		if formProprietario.is_valid():
+			formProprietario.save()
+		else:
+			formProprietario=proprietarioForm()
+	return render(request,'formulario_proprietario.html',{'formProprietario':formProprietario})
+
+def condominioFormulario(request):
+	formCondominio=condominioForm(request.POST or None)
+	if request=='POST':
+		if formCondominio.is_valid():
+			formCondominio.save()
+		else:
+			formCondominio=condominioForm()
+	return render(request,'formulario_condominio.html',{'formCondominio':formCondominio})
+
+def inicio(request):
+	return render(request,'tela_inicial.html')
